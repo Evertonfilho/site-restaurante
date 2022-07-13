2d7ada5d0853f04749d16559e8e1f406ecd78939
Everton Filho <evertonfilho0513@gmail.com>
2022-07-13 14:02:44 -0300

    feat:commit projeto

---
+++ src/pages/Cardapio/Filtros/index.tsx
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable array-callback-return */
+import { OperationCanceledException } from 'typescript'
+import filtros from './filtros.json'
+import styles from './Filtros.module.scss'
+import classNames from 'classnames'
+type IOpcao = typeof filtros[0]
+
+interface PropsFiltro {
+    filtro:number|null,
+    setFiltro:React.Dispatch<React.SetStateAction<number|null>>
+}
+
+export default function Filtros({filtro,setFiltro}:PropsFiltro){
+    function selecionarFiltro(opcao:IOpcao){
+        if(filtro === opcao.id) return setFiltro(null)
+        return setFiltro(opcao.id)
+    }
+    return(
+        <div className={styles.filtros}>
+            {filtros.map((opcao) => (
+                <button 
+                className={classNames({
+                    [styles.filtros__filtro]: true,
+                    [styles["filtros__filtro--ativo"]]:filtro === opcao.id,
+                })}
+                key={opcao.id}
+                onClick={() => selecionarFiltro(opcao)}
+                >
+                {opcao.label}
+                </button>
+            ))}
+        </div>
+    )
+}
+
